6f1cbfd9df457e1595bd5679ea81684ece010517
Andreas Achte <andreas.achte@gmail.com>
2022-06-28 20:30:30 +0300

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 // current index of typing
 let index = 0
+let wrong = 0
 // current length of things in front of text:
 let currentLength = 1
 const theText = document.getElementById("write")
 const textContent = "this is a typing game try to type all the characters as fast as possible"
-
 
 // wrap all characters in a span element to change the color:
 textContent.split('').forEach(character => {
@@ -20,33 +20,42 @@ chars[0].id = 'current'
 
 // When a user presses a key:
 document.addEventListener("keydown", function(event) {
-    //start timer at first press:
-    if(index == 0){
-        timer();
-    }
-    if(index < textContent.length){
-        currentChar = textContent.charAt(index)
-        // check if key pressed was correct:
-        if(currentChar == event.key){
-            chars[index].className = 'correct'
+    // if key pressed was a single letter, we do something:
+    if(event.key.length == 1){
+        //start timer at first press:
+        if(index == 0){
+            timer(1);
         }
-        else{
-            chars[index].className = 'incorrect'
+        // do something only if we have not reached the end of text:
+        if(index < textContent.length){
+            currentChar = textContent.charAt(index)
+            // check if key pressed was correct:
+            if(currentChar == event.key){
+                chars[index].className = 'correct'
+            }
+            else{
+                chars[index].className = 'incorrect'
+                wrong++
+                document.getElementById('wrong').innerHTML = "mistakes: " + wrong
+            }
+            index++
+            //special case check: if we reached last letter
+            // then we dont add the line id anymore
+            if(index != textContent.length){
+                //move the line that shows current index:
+                chars[index].id = 'current'
+                // stop timer at last letter:
+            }
+            // remove id from the last element
+            chars[index-1].id = ''
         }
-        //move the line that shows current index:
-        // remove class from the last element
-        index++
-        chars[index].id = 'current'
-        chars[index-1].id = ''
     }
 })
-
-
 
 function timer() {
     let sec = 0
     let timer = setInterval(function () {
-        document.getElementById('timer').innerHTML = sec
-        sec++
-    }, 1000)
+        document.getElementById('timer').innerHTML = "Seconds: " + sec
+    sec++
+        }, 1000)
 }
